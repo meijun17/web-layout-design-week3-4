e30b4060e00362a99c13d07e730beef5b120630e
Mei Jun Chen <wdxlf8992@gmail.com>
2022-08-04 01:10:33 +0800

    add

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,7 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$('.navBtn').on('click', () => {
+    $('.nav').toggleClass('active');
 });
+
+// $('.nav__link').on('click', () => {
+//     $(this).addClass('.active');
+// });
